949db11fe716f4f9bbf13dc1bd93139465c9138b
gaberussell <gabe.russell@gmail.com>
2017-12-13 13:28:19 -0800

    Fixed spacing issue

--- watchlist.js
+++ watchlist.js
@@ -15,12 +15,12 @@ var show_cache = [];
 
 // Click handler for search button
 function searchShows() {
-	// Get contents of search box and query tvmaze API
-	$.get(search_url + $('#searchBox').val(), function(response) { 
-		// show results and add to cache
-		listShows(response);
-		cacheShows(response);
-	});
+    // Get contents of search box and query tvmaze API
+    $.get(search_url + $('#searchBox').val(), function(response) { 
+        // show results and add to cache
+        listShows(response);
+        cacheShows(response);
+    });
 }
 
 // Populate show list from API data
@@ -32,68 +32,68 @@ function listShows(data) {
 
 // Cache show details from search for reuse in watchlist, saving API hits
 function cacheShows(data) {
-	show_cache = [];
-	for (item of data) {
-		show_cache.push(item.show);
-	}
+    show_cache = [];
+    for (item of data) {
+        show_cache.push(item.show);
+    }
 }
 
 // Get episode and cast list and popup a modal
 function showDetails(id) {
-	$.get(shows_url + id + '?embed[]=episodes&embed[]=cast', function(response) {
-		var details_tmpl = $.templates("#detailsModalTmpl");
-	    var details_html = details_tmpl.render(response);
-	    $('#detailsModalContainer').html(details_html);  
-		$('#detailsModal').modal();
-	});
+    $.get(shows_url + id + '?embed[]=episodes&embed[]=cast', function(response) {
+        var details_tmpl = $.templates("#detailsModalTmpl");
+        var details_html = details_tmpl.render(response);
+        $('#detailsModalContainer').html(details_html);  
+        $('#detailsModal').modal();
+    });
 }
 
 // Popup a modal with watchlist
 function showWatchlist() {
-	if (watchlist.length < 1) alert("Your watchlist is empty. Add some shows to get started!");
-	else {
-		$('#watchlistModal').modal();
-	}
+    if (watchlist.length < 1) alert("Your watchlist is empty. Add some shows to get started!");
+    else {
+        $('#watchlistModal').modal();
+    }
 }
 
 // Add show to watchlist from cache if it's not already there
 function addWatchlist(id) {
-	if (watchlist.findIndex(function(e){return e.id == id}) == -1) {
-		show = $.grep(show_cache, function(e){return e.id == id})[0];
-		show.watched = false;
-		watchlist.push(show);
-	}
-	updateWatchlistModal();
+    if (watchlist.findIndex(function(e){return e.id == id}) == -1) {
+        show = $.grep(show_cache, function(e){return e.id == id})[0];
+        show.watched = false;
+        watchlist.push(show);
+    }
+    updateWatchlistModal();
 }
 
 // Remove show from watchlist
 function removeWatchList(id) {
-	watchlist = watchlist.filter(function(e){return e.id !== id});
-	updateWatchlistModal();
+    watchlist = watchlist.filter(function(e){return e.id !== id});
+    updateWatchlistModal();
 }
 
 // Update the watchlist view
 function updateWatchlistModal() {
-	var watchlist_tmpl = $.templates("#watchlistModalTmpl");
+    var watchlist_tmpl = $.templates("#watchlistModalTmpl");
     var watchlist_html = watchlist_tmpl.render(watchlist);
     $('#watchlistModalContent').html(watchlist_html);  
 }
 
 // Handle "watched" checkbox clicks and update watchlist
 function updateWatched(id,element) {
-	index = watchlist.findIndex(function(e){return e.id == id});
-	watchlist[index].watched = element.checked;
+    index = watchlist.findIndex(function(e){return e.id == id});
+    watchlist[index].watched = element.checked;
 }
 
 // Assign search handler to search button and enter key in text box
 $(function() {
-	$('#searchButton').click(searchShows);
-	$('#searchBox').keypress(function (e) {
-	 var key = e.which;
-	 if(key == 13)  // enter key 
-	  {
-	    searchShows();
-	    return false;  
-	  }
-	});   	
+    $('#searchButton').click(searchShows);
+    $('#searchBox').keypress(function (e) {
+     var key = e.which;
+     if(key == 13)  // enter key 
+      {
+        searchShows();
+        return false;  
+      }
+    });     
 });
